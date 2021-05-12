2db92e0350f11400f40cc80f70073231d2fd6e01
jesperu7 <jesperu7@hotmail.com>
2021-05-12 13:07:45 +0200

    init

---
+++ oppgave/www/js/components/edit-user.js
@@ -0,0 +1,16 @@
+import {
+  LitElement,
+  html,
+  css,
+} from "../../node_modules/lit-element/lit-element.js";
+
+class EditUser extends LitElement {
+  static get properties() {
+    return {
+      user: { type: Object },
+    };
+  }
+
+  // din kode her
+}
+customElements.define("edit-user", EditUser);
